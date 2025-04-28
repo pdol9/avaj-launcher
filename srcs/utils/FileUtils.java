88647a635a33a28813460790eb80f4ff6bcb6a32
pdol <111592903+pdol9@users.noreply.github.com>
2025-04-28 15:21:15 +0200

    fix error handling for input file

--- srcs/utils/FileUtils.java
+++ srcs/utils/FileUtils.java
@@ -9,29 +9,49 @@ import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class FileUtils {
-    public static List<String> readFile(String fileName) throws IOException {
+    static String inputFile = "scenario.txt";
+    public static List<String> readFile(String fileName) {
+        // Check if fileName is null or empty
+        if (fileName == null || fileName.trim().isEmpty()) {
+            System.err.println("Error: No input file provided.");
+            System.exit(1);
+        }
+
+        if (!fileName.equals(inputFile)) {
+            System.err.println("Error: Input file must be named " + inputFile);
+            System.exit(1);
+        }
+
         Path filePath = Paths.get(fileName);
-        List<String> lines;
+        List<String> lines = null;
+
+        // Check if file exists
+        if (!Files.exists(filePath)) {
+            System.err.println("Error: File '" + inputFile + "' does not exist.");
+            System.exit(1);
+        }
+
         try {
             lines = Files.readAllLines(filePath);
             if (lines.isEmpty()) {
-                throw new IOException(fileName + " is empty");
+                System.err.println("Error: File '" + inputFile + "' is empty.");
+                System.exit(1);
             }
         } catch (IOException e) {
-            System.err.format("IOException while reading scenario.txt: %s%n", e);
-            throw e;
+            System.err.println("Error: Unable to read " + inputFile);
+            System.exit(1);
         }
+
         return lines;
     }
 
     public static PrintStream redirectOutputToFile() throws IOException {
-        Path newFilePath = Paths.get("./simulation.txt");
+        Path newFilePath = Paths.get("simulation.txt");
 
         PrintStream fileOut = new PrintStream(
             Files.newOutputStream(
                 newFilePath,
-                StandardOpenOption.CREATE,
-                StandardOpenOption.TRUNCATE_EXISTING
+                StandardOpenOption.CREATE
             ),
             true,
             "UTF-8"
